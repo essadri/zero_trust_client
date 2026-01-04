03ec75b2d5a78cac33bb221ff9c627cd4c1ed3b2
Your Name <you@example.com>
2026-01-04 13:31:53 +0000

    moveing from root

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import https from "https";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Needed for Docker
+    host: true,
     proxy: {
-      '/api': {
-        target: 'http://10.114.0.3:5000', // ✅ Use APP SERVER PRIVATE IP
+      "/api": {
+        target: "https://10.114.0.3:5000",
         changeOrigin: true,
         secure: false,
+        agent: new https.Agent({
+          cert: fs.readFileSync("./certs/client.crt"),
+          key: fs.readFileSync("./certs/client.key"),
+          ca: fs.readFileSync("./certs/ca.crt"),
+        }),
+        headers: {
+          "x-internal-secret": "superSecretPassword123!",
+        },
       },
     },
   },
-})
+});
+
